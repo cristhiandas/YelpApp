544b84d9674fd8d1d78283f4a9038ac85215e767
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-11 17:47:13 +0000

    added a min and max field for new restaurant form

--- spec/features/new.html.erb_features_spec.rb
+++ spec/features/new.html.erb_features_spec.rb
@@ -26,5 +26,14 @@ RSpec.describe "restaurants/new.html.erb" do
     it 'has a location field' do
       expect(page).to have_field(:type => 'text', :name => 'restaurant[location]')
     end
+
+    it 'has a min price field for price range' do
+      expect(page).to have_field(:type => 'text', :name => 'restaurant[min_price]')
+    end
+
+    it 'has a max price field for price range' do
+      expect(page).to have_field(:type => 'text', :name => 'restaurant[max_price]')
+    end
   end
+
 end
