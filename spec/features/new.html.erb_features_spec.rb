389301d9373f8d5897589b9e6f4dcfc46be854fb
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-11 17:37:56 +0000

    added a location field to the restaurant new form

--- spec/features/new.html.erb_features_spec.rb
+++ spec/features/new.html.erb_features_spec.rb
@@ -2,24 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "restaurants/new.html.erb" do
   describe 'when opening the page' do
-    it "has content New Restaurant" do
+
+    before(:each) do
       visit('/restaurants/new')
+    end
+
+    it "has content New Restaurant" do
       expect(page).to have_content('New Restaurant')
     end
 
     it 'has a name field' do
-      visit('/restaurants/new')
       expect(page).to  have_field(:type => 'text', :name => 'restaurant[name]')
     end
 
     it 'has a description field' do
-      visit('/restaurants/new')
       expect(page).to  have_field(:type => 'textarea', :name => 'restaurant[description]')
     end
 
     it 'has a submit button' do
-      visit('/restaurants/new')
       expect(page).to have_button(:value => 'Save Restaurant')
+    end
+
+    it 'has a location field' do
+      expect(page).to have_field(:type => 'text', :name => 'restaurant[location]')
     end
   end
 end
