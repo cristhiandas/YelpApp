a9ee6a75e8c970f3349bc63f8bf2932232ef23e5
cristhiandas <cristhiandf94@gmail.com>
2017-12-11 17:30:34 +0000

    added creation form

--- spec/features/new.html.erb_features_spec.rb
+++ spec/features/new.html.erb_features_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "restaurants/new.html.erb" do
-  it "has content New Restaurant" do
-    visit('/restaurants/new')
-    expect(page).to have_content('New Restaurant')
+  describe 'when opening the page' do
+    it "has content New Restaurant" do
+      visit('/restaurants/new')
+      expect(page).to have_content('New Restaurant')
+    end
+
+    it 'has a name field' do
+      visit('/restaurants/new')
+      expect(page).to  have_field(:type => 'text', :name => 'restaurant[name]')
+    end
+
+    it 'has a description field' do
+      visit('/restaurants/new')
+      expect(page).to  have_field(:type => 'textarea', :name => 'restaurant[description]')
+    end
+
+    it 'has a submit button' do
+      visit('/restaurants/new')
+      expect(page).to have_button(:value => 'Save Restaurant')
+    end
   end
 end
