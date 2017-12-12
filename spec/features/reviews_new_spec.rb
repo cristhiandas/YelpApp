faf2b50f0f9c2d1c3ac75e5a908b8353418462de
Lewis <lewisyoul@gmail.com>
2017-12-12 15:14:22 +0000

    Review Form
    create review author, email and rating

--- spec/features/reviews_new_spec.rb
+++ spec/features/reviews_new_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 feature 'Post a Review' do
-  scenario 'can visit the page' do
+
+  before(:each) do
     visit('/reviews/new')
+  end
+
+  scenario 'can visit the page' do
     expect(page).to have_content('Post a Review')
+  end
+
+  scenario 'Filling in the name' do
+    expect(page).to have_field(type: 'text', name: 'review[author]')
+  end
+
+  scenario 'Filling in the email' do
+    expect(page).to have_field(type: 'email', name: 'review[email]')
+  end
+
+  scenario 'Filling in the rating' do
+    expect(page).to have_field(id: 'review_rating_1')
   end
 end
