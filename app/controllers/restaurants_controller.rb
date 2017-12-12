96637cfee985a2af046623605c9c6751afc734d8
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-12 15:09:16 +0000

    Saves new restaurant to DB
    restaurants params are now saved via the restaurants controller when a new restaurant is summited. A private method restaurant_params was also created which have been made pre-emptivly to DRY out the code.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,15 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    render plain: params[:restaurant].inspect
+    # render plain: params[:restaurant].inspect
+    @restaurant = Restaurant.new(restaurant_params)
+
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location, :min_price, :max_price, :description, :catergory)
   end
 end
