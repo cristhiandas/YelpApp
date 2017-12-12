c0378050745b9cb877223962d825d9b733dd49d1
Lewis <lewisyoul@gmail.com>
2017-12-12 17:32:26 +0000

    create page to show created review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,18 @@
 class ReviewsController < ApplicationController
 
+  def show
+    @review = Review.find(params[:id])
+  end
+
   def new
   end
 
   def create
-
     @review = Review.new(review_params)
     @review.save
-
     redirect_to @review
-
   end
+
 
   private
 
