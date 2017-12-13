4202dc5f160e1d40bd13aea1b862ffe02cf6ebc6
Lewis <lewisyoul@gmail.com>
2017-12-13 17:15:55 +0000

    add review form validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  validates :author, presence: true,
+                     length: { minimum: 2 }
+  validates :rating, presence: true
+
   belongs_to :restaurant
 end
