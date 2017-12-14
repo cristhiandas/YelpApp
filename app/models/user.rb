b0b0dc94111f92e5949b863a4c458715e6c2c25f
Robert Clayton <robbie.c.clayton@gmail.com>
2017-12-14 16:50:41 +0000

    now have to enter name on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :restaurants
 
+  validates :name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
